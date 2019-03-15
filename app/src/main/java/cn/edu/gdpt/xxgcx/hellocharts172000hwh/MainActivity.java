57ff2beba62a1a1ec3bfd3b52ed462815f5c2c54
wonder0112 <hwhgdcn@gmail.com>
2019-03-15 09:11:16 +0800

    控件初始化

--- app/src/main/java/cn/edu/gdpt/xxgcx/hellocharts172000hwh/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/xxgcx/hellocharts172000hwh/MainActivity.java
@@ -1,13 +1,24 @@
 package cn.edu.gdpt.xxgcx.hellocharts172000hwh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayoutMainTabs;
+    private ViewPager mViewPagerMainCharts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTabLayoutMainTabs = (TabLayout) findViewById(R.id.tabLayout_main_tabs);
+        mViewPagerMainCharts = (ViewPager) findViewById(R.id.viewPager_main_charts);
     }
 }
